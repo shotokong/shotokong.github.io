d423ea64eeb1408d23551b68a7904788bb6c5faa
shotokong <shotokong@gmail.com>
2019-04-24 14:44:39 -0400

    all

---
+++ Rest3/ContactRest.js
@@ -0,0 +1,24 @@
+
+  function verify() {
+    let name = document.getElementById('name').value;
+    let addy = document.getElementById('addy').value;
+
+
+    if (name == "" ){
+       alert("please enter your full name");
+
+       return false;
+
+      }
+
+
+          if (addy == ""){
+             alert("please enter your address");
+
+             return false;
+            }
+
+
+return false;
+
+     }
